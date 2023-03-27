3c90c09909f8c9e24d1bcbba9dee419b89ee2ab2
Seth Corker <seth.corker@gmail.com>
2023-03-27 15:45:23 +0100

    use clap for args

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,7 +1,20 @@
+use clap::Parser;
 use regex::Regex;
 
+#[derive(Parser)]
+#[command(name = "Grep Lite")]
+#[command(author = "Seth C. <hello@sethcorker.com>")]
+#[command(version = "0.1")]
+#[command(about = "A terrible grep implementation in rust", long_about = None)]
+struct Args {
+    /// The pattern to search for
+    pattern: String,
+}
+
 fn main() {
-    let regexp = Regex::new("picture").unwrap();
+    let cli = Args::parse();
+
+    let regexp = Regex::new(cli.pattern.as_str()).unwrap();
     let quote = "\
     Every face, every shop, bedroom windor, public-house, and
     dark square is a picture feverishly turned--in search of what?
